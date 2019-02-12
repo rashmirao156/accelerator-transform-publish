03dc91b706773e13a657598881afb8b698fff936
Rashmi R <rashmirao156@gmail.com>
2019-02-12 18:51:04 +0530

    intitial commit

---
+++ src/test/java/com/deloitte/sample/integration/transformerpublish/TransformerPublishApplicationTests.java
@@ -0,0 +1,17 @@
+package com.deloitte.sample.integration.transformerpublish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransformerPublishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
